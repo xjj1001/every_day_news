a8dc20d03d945e4a821ae53a1ca140b442934176
Xu Jianjun <38558238+xjj1001@users.noreply.github.com>
2023-10-27 11:54:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "666666"
+cat add.csv
 echo "1,2,3" >> add.csv
 git status
 git add add.csv
 git commit -m 'update add.csv'
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 git push origin main
+
