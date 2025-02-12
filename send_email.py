c44a2d9ca272ef291b9f016cc01b4b06c4db8b02
Xu Jianjun <38558238+xjj1001@users.noreply.github.com>
2025-02-12 10:25:13 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,22 +1,44 @@
-import smtplib, ssl
-import os
-
-port = 465
-smtp_server = "smtp.qq.com"
-USER_EMAIL = os.environ.get("USER_EMAIL")
-USER_PASSWORD = os.environ.get("USER_PASSWORD")
-
-print(USER_EMAIL, USER_PASSWORD)
-
-message = """\
-    Subject: Welcome Ubaydah
-
-    This is your welcome email running 
+# smtplib 用于邮件的发信动作
+import smtplib
+# email 用于构建邮件内容
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+# 构建邮件头
+from email.header import Header
+ 
+ 
+# 发信方的信息：发信邮箱，QQ 邮箱授权码
+from_addr = os.environ.get("USER_EMAIL")
+password = os.environ.get("USER_PASSWORD")
+# 收信方邮箱
+to_addr = from_addr
+# 发信服务器
+smtp_server = 'smtp.qq.com'
+ 
+html_msg = """
+<p>Steam 今日游戏促销...</p>
+<p><a href="https://store.steampowered.com/specials/">这是一个链接</a></p>
 """
-
-context = ssl.create_default_context()
-
-server = smtplib.SMTP_SSL(smtp_server, port, context=context)
-
-server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(USER_EMAIL, USER_EMAIL, message)
+ 
+ 
+# 创建一个实例msg
+msg = MIMEMultipart()
+msg['From'] = '"John Doe" <xxx>' # 发送者
+msg['To'] = Header('到点就困告')  # 接收者
+subject = 'steam今日促销'
+msg['Subject'] = Header(subject, 'utf-8')  # 邮件主题
+# 邮件正文内容
+msg.attach(MIMEText(html_msg, 'html', 'utf-8'))
+ 
+ 
+try:
+    smtpobj = smtplib.SMTP_SSL(smtp_server)
+    smtpobj.connect(smtp_server, 465)    # 建立连接--qq邮箱服务和端口号
+    smtpobj.login(from_addr, password)   # 登录--发送者账号和口令
+    smtpobj.sendmail(from_addr, to_addr, msg.as_string())
+    print("邮件发送成功")
+except smtplib.SMTPException as e:
+    print("无法发送邮件:", e)
+finally:
+    # 关闭服务器
+    smtpobj.quit()
