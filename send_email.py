e08c957a22316ec17b7d79424d62d6cda289c6f1
Xu Jianjun <38558238+xjj1001@users.noreply.github.com>
2025-09-04 12:04:16 +0800

    Update email subject with trending repositories

--- send_email.py
+++ send_email.py
@@ -19,17 +19,50 @@ html_msg = """
 <p>Steam 今日游戏促销...</p>
 <p><a href="https://store.steampowered.com/specials/">这是一个链接</a></p>
 """
- 
+
+def get_trends():
+    import requests
+   
+    headers = {
+        "accept": "text/html, application/xhtml+xml",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN, zh; q=0.9, en; q=0.8",
+        "cache-control": "no-cache",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+    }
+    
+    response = requests.get('https://github.com/trending')
+    # 解析热榜项目链接和摘要
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    repo_list = soup.find_all('article', class_='Box-row')
+    trends = []
+    for repo in repo_list:
+        title_tag = repo.h2.a
+        title = title_tag.get_text(strip=True).replace('\n', '').replace(' ', '')
+        link = 'https://github.com' + title_tag['href']
+        description_tag = repo.find('p', class_='col-9 color-fg-muted my-1 pr-4')
+        description = description_tag.get_text(strip=True) if description_tag else 'No description'
+        # 获取语言标签
+        language_tag = repo.find('span', itemprop='programmingLanguage')
+        language = language_tag.get_text(strip=True) if language_tag else 'No language'
+        trends.append({'title': title, 'link': link, 'description': description, 'language': language})
+     return trends
+
+trend_list = get_trends()
+# 用trends拼接一个邮件内容
+email_content = "GitHub Trending Repositories:\n\n"
+for news in trends:
+    email_content += f"Language: {news['language']}\nTitle: {news['title']}\nLink: {news['link']}\nDescription: {news['description']}\n\n"
  
 # 创建一个实例msg
 msg = MIMEMultipart()
 msg['From'] = f'"xujianjun" <{from_addr}>' # 发送者
 msg['To'] = Header('到点就困告')  # 接收者
-subject = 'steam今日促销'
+subject = email_content
 msg['Subject'] = Header(subject, 'utf-8')  # 邮件主题
 # 邮件正文内容
 msg.attach(MIMEText(html_msg, 'html', 'utf-8'))
- 
  
 try:
     smtpobj = smtplib.SMTP_SSL(smtp_server)
