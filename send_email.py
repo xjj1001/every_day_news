d788ae2add625f6a9b7e601429aa4e41b381845f
Xu Jianjun <38558238+xjj1001@users.noreply.github.com>
2025-09-04 12:08:21 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,8 +5,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 # 构建邮件头
 from email.header import Header
- 
- 
+
 # 发信方的信息：发信邮箱，QQ 邮箱授权码
 from_addr = os.environ.get("USER_EMAIL")
 password = os.environ.get("USER_PASSWORD")
@@ -14,15 +13,11 @@ password = os.environ.get("USER_PASSWORD")
 to_addr = from_addr
 # 发信服务器
 smtp_server = 'smtp.qq.com'
- 
-html_msg = """
-<p>Steam 今日游戏促销...</p>
-<p><a href="https://store.steampowered.com/specials/">这是一个链接</a></p>
-"""
+
 
 def get_trends():
     import requests
-   
+
     headers = {
         "accept": "text/html, application/xhtml+xml",
         "accept-encoding": "gzip, deflate, br, zstd",
@@ -30,7 +25,7 @@ def get_trends():
         "cache-control": "no-cache",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
     }
-    
+
     response = requests.get('https://github.com/trending')
     # 解析热榜项目链接和摘要
     from bs4 import BeautifulSoup
@@ -47,27 +42,28 @@ def get_trends():
         language_tag = repo.find('span', itemprop='programmingLanguage')
         language = language_tag.get_text(strip=True) if language_tag else 'No language'
         trends.append({'title': title, 'link': link, 'description': description, 'language': language})
-     return trends
+    return trends
+
 
 trend_list = get_trends()
 # 用trends拼接一个邮件内容
 email_content = "GitHub Trending Repositories:\n\n"
-for news in trends:
-    email_content += f"Language: {news['language']}\nTitle: {news['title']}\nLink: {news['link']}\nDescription: {news['description']}\n\n"
- 
+for trend in trend_list:
+    email_content += f"Language: {trend['language']}\nTitle: {trend['title']}\nLink: {trend['link']}\nDescription: {trend['description']}\n\n"
+
 # 创建一个实例msg
 msg = MIMEMultipart()
-msg['From'] = f'"xujianjun" <{from_addr}>' # 发送者
+msg['From'] = f'"xujianjun" <{from_addr}>'  # 发送者
 msg['To'] = Header('到点就困告')  # 接收者
 subject = email_content
 msg['Subject'] = Header(subject, 'utf-8')  # 邮件主题
 # 邮件正文内容
-msg.attach(MIMEText(html_msg, 'html', 'utf-8'))
- 
+msg.attach(MIMEText(email_content, 'plain', 'utf-8'))
+
 try:
     smtpobj = smtplib.SMTP_SSL(smtp_server)
-    smtpobj.connect(smtp_server, 465)    # 建立连接--qq邮箱服务和端口号
-    smtpobj.login(from_addr, password)   # 登录--发送者账号和口令
+    smtpobj.connect(smtp_server, 465)  # 建立连接--qq邮箱服务和端口号
+    smtpobj.login(from_addr, password)  # 登录--发送者账号和口令
     smtpobj.sendmail(from_addr, to_addr, msg.as_string())
     print("邮件发送成功")
 except smtplib.SMTPException as e:
