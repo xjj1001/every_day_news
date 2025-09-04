b7455a9751608607c951b502d530bfc52d259474
Xu Jianjun <38558238+xjj1001@users.noreply.github.com>
2025-09-04 19:16:37 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,18 +5,68 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 # 构建邮件头
 from email.header import Header
+import requests
 
 # 发信方的信息：发信邮箱，QQ 邮箱授权码
 from_addr = os.environ.get("USER_EMAIL")
 password = os.environ.get("USER_PASSWORD")
-get_trends_str = os.environ.get("GET_TRENDS")
-print(get_trends_str)
 # 收信方邮箱
 to_addr = from_addr
 # 发信服务器
 smtp_server = 'smtp.qq.com'
-exec(get_trends_str)
-trend_list = eval('get_trends()')
+
+
+def get_trendsv2():
+    headers = {
+        "accept": "text/html, application/xhtml+xml",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN, zh; q=0.9, en; q=0.8",
+        "cache-control": "no-cache",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+    }
+    response = requests.get('https://trendforge.devlive.org/trending/daily', headers=headers)
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    repo_list = soup.find('div', class_=['grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6']).find_all('div', class_='p-6')
+    trends = []
+    for repo in repo_list:
+        title = repo.find('span', class_='truncate').get_text(strip=True)
+        link = repo.find('h2', class_='text-xl').find('a').get('href')
+        language = repo.find('span', class_=['px-2', 'py-1', 'bg-gray-100']).get_text(strip=True)
+        description = repo.find('p').get_text(strip=True)
+        trends.append({'title': title, 'link': link, 'description': description, 'language': language})
+    return trends
+
+
+def get_trends():
+    headers = {
+        "accept": "text/html, application/xhtml+xml",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN, zh; q=0.9, en; q=0.8",
+        "cache-control": "no-cache",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get('https://github.com/trending', headers=headers)
+    # 解析热榜项目链接和摘要
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    repo_list = soup.find_all('article', class_='Box-row')
+    trends = []
+    for repo in repo_list:
+        title_tag = repo.h2.a
+        title = title_tag.get_text(strip=True).replace('\n', '').replace(' ', '')
+        link = 'https://github.com' + title_tag['href']
+        description_tag = repo.find('p', class_='col-9 color-fg-muted my-1 pr-4')
+        description = description_tag.get_text(strip=True) if description_tag else 'No description'
+        # 获取语言标签
+        language_tag = repo.find('span', itemprop='programmingLanguage')
+        language = language_tag.get_text(strip=True) if language_tag else 'No language'
+        trends.append({'title': title, 'link': link, 'description': description, 'language': language})
+    return trends
+
+
+trend_list = get_trendsv2()
 # 用trend_list拼接一个html邮件内容
 email_content = "<h2>GitHub 热榜:</h2><br>"
 for trend in trend_list:
@@ -26,7 +76,7 @@ for trend in trend_list:
 msg = MIMEMultipart()
 msg['From'] = f'"xujianjun" <{from_addr}>'  # 发送者
 msg['To'] = Header('到点就困告')  # 接收者
-subject = "Github热榜"
+subject = 'Github热榜'
 msg['Subject'] = Header(subject, 'utf-8')  # 邮件主题
 # 邮件正文内容
 msg.attach(MIMEText(email_content, 'html', 'utf-8'))
